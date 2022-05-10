8bbae8de58648b620638d6acb5d6418783afe601
Sam <100165896+miss-sam@users.noreply.github.com>
2022-05-10 13:04:30 -0600

    Create HadesQuotes.py

---
+++ HadesQuotes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Hadestown + Percy Jackson Quotes
+
+Hades = ["Name\'s Hades, Lord of the dead. Hi, how ya doin\'?", "Are you calling me a minor god?", "I can\'t believe this guy! I throw everything I\'ve got at him, and it doesn't even...", "Uh, guys? Olympus would be that way.", "YES! Hades rules!", "It\'s a small underworld, after all, huh?", "My favorite part of the game: sudden death. Game, set, match."]
+
+Eurydice = ["To the world we dream about and the one we live in now.", "See how the world could be, in spite of the way that it is.", "Livin\' it up on top!", "Meanest dog you’ll ever meet? It ain’t the hound dog in the street. He bares his teeth and tears your skin. But brother, that’s the worst of him. The dog you really got to dread is the one that howls inside your head. It\'s him whose howling drives men mad and a mind to its undoing."]
